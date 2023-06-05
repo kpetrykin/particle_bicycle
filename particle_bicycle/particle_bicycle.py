3a5d51c9ca92926839e5fcb5a79f11dbf1171c8c
Konstantin Petrykin <kpetrykin@gmail.com>
2023-06-05 13:44:51 +0000

    Fix format, remove artefact

--- particle_bicycle/particle_bicycle.py
+++ particle_bicycle/particle_bicycle.py
@@ -19,36 +19,36 @@ class ParticleBicycle(Node):
 
         self._robot = robot
         self._particle_filter = particle_filter
-        
+
         self._init_filter()
-        
+
         self.create_subscription(
             Float32MultiArray, 'move_robot', self._move_robot_cb, 1)
 
         self.gt_pub = self.create_publisher(PoseStamped, 'ground_truth', 10)
         self.est_pose_pub = self.create_publisher(PoseStamped, 'position', 10)
-        
+
     def _init_filter(self):
         ref_sensed = self._robot.sense()
 
         self._particle_filter.particles_sense()
         self._particle_filter.update_particles_weights(ref_sensed)
-        
+
     def _estimate_pose_after_move(self, dist, steer):
         # Moving phase
         self._particle_filter.move_particles(dist, steer)
-        
+
         # Measurement phase
         ref_sensed = self._robot.sense()
         self._particle_filter.particles_sense()
         self._particle_filter.update_particles_weights(ref_sensed)
-        
+
         # Resampling phase
         self._particle_filter.resample()
-        
+
         # Estimation phase
         est_x, est_y, est_heading = self._particle_filter.estimate_pose()
-        
+
         return est_x, est_y, est_heading
 
     def _move_robot_cb(self, msg):
@@ -60,32 +60,26 @@ class ParticleBicycle(Node):
         q = Quaternion(x=x, y=y, z=z, w=w)
         p = Point(x=self._robot.x, y=self._robot.y, z=0.0)
         rob_pose = Pose(position=p, orientation=q)
-        
+
         h = Header()
         h.frame_id = "base_link"
         rob_pose_stamped = PoseStamped(header=h, pose=rob_pose)
         self.gt_pub.publish(rob_pose_stamped)
 
-
         # Publishing estimated position
-        est_x, est_y, est_heading = self._estimate_pose_after_move(msg.data[0], msg.data[1])
-        
+        est_x, est_y, est_heading = self._estimate_pose_after_move(
+            msg.data[0], msg.data[1])
+
         x, y, z, w = tf_transformations.quaternion_from_euler(
             0, 0, est_heading)
         q = Quaternion(x=x, y=y, z=z, w=w)
         p = Point(x=est_x, y=est_y, z=0.0)
         est_pose = Pose(position=p, orientation=q)
-        
+
         h = Header()
         h.frame_id = "base_link"
         est_pose_stamped = PoseStamped(header=h, pose=est_pose)
         self.est_pose_pub.publish(est_pose_stamped)
-        
-
-    def timer_callback(self):
-        my_msg = Float32MultiArray()
-        my_msg.data = [10.0, 1.5]
-        self.mu_pub.publish(my_msg)
 
 
 def main(args=None):
